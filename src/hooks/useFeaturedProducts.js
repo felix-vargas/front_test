1698fcf1d07b133d4cf9008baff9775ef5126918
Felix <felix.vargas@sansano.usm.cl>
2024-03-20 01:46:34 -0300

    ,

---
+++ src/hooks/useFeaturedProducts.js
@@ -0,0 +1,61 @@
+import { useDidMount } from '@/hooks';
+import { useEffect, useState } from 'react';
+import firebase from '@/services/firebase';
+import axios from 'axios';
+
+const useFeaturedProducts = (itemsCount) => {
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const didMount = useDidMount(true);
+
+  const fetchFeaturedProducts = async () => {
+    try {
+      setLoading(true);
+      setError('');
+
+      //const docs = await firebase.getFeaturedProducts(itemsCount);
+
+      const result = await axios.get('http://localhost:8000/api/productos');      
+      console.log('--------');
+      console.log(result);
+      console.log('--------');
+      let docs = result.data;
+      console.log(docs);
+      if (docs.empty) {
+        if (didMount) {
+          setError('No featured products found.');
+          setLoading(false);
+        }
+      } else {
+        const items = docs;
+
+        docs.forEach((doc) => {
+          items.push({ id: doc.id, ...doc });
+        });
+
+        if (didMount) {
+          setFeaturedProducts(items);
+          setLoading(false);
+        }
+      }
+    } catch (e) {
+      if (didMount) {
+        setError('Failed to fetch featured products');
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (featuredProducts.length === 0 && didMount) {
+      fetchFeaturedProducts();
+    }
+  }, []);
+
+  return {
+    featuredProducts, fetchFeaturedProducts, isLoading, error
+  };
+};
+
+export default useFeaturedProducts;
---
+++ src/views/featured/index.jsx
@@ -0,0 +1,64 @@
+import { MessageDisplay } from '@/components/common';
+import { ProductShowcaseGrid } from '@/components/product';
+import { useDocumentTitle, useFeaturedProducts, useScrollTop } from '@/hooks';
+import bannerImg from '@/images/banner-guy.png';
+import React from 'react';
+
+const FeaturedProducts = () => {
+  useDocumentTitle('Featured Products | Salinaka');
+  useScrollTop();
+
+  let {
+    featuredProducts,
+    fetchFeaturedProducts,
+    isLoading,
+    error
+  } = useFeaturedProducts();
+
+  const uniqueIds = {};
+
+  // Filter out objects with duplicate IDs
+  featuredProducts = featuredProducts.filter(obj => {
+      // If the ID is not in the uniqueIds object, add it and return true to keep the object
+      if (!uniqueIds[obj.id]) {
+          uniqueIds[obj.id] = true;
+          return true;
+      }
+      // If the ID is already in the uniqueIds object, return false to filter out the object
+      return false;
+  });
+
+
+  return (
+    <main className="content">
+      <div className="featured">
+        <div className="banner">
+          <div className="banner-desc">
+            <h1>Featured Products</h1>
+          </div>
+          <div className="banner-img">
+            <img src={bannerImg} alt="" />
+          </div>
+        </div>
+        <div className="display">
+          <div className="product-display-grid">
+            {(error && !isLoading) ? (
+              <MessageDisplay
+                message={error}
+                action={fetchFeaturedProducts}
+                buttonLabel="Try Again"
+              />
+            ) : (
+              <ProductShowcaseGrid
+                products={featuredProducts}
+                skeletonCount={6}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default FeaturedProducts;
