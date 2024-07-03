183af9341f446ab2cf628467b840538e1602bfe4
Felix <felix.vargas@sansano.usm.cl>
2024-07-03 01:32:29 -0400

    .

--- src/views/checkout/step3/Total.jsx
+++ src/views/checkout/step3/Total.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { setPaymentDetails } from '@/redux/actions/checkoutActions';
+import axios from 'axios';
 
 const Total = ({ isInternational, subtotal }) => {
   const { values, submitForm } = useFormikContext();
